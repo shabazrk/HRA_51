6580c82cc83b0c0a52983b9a7f87df91dd711b14
Admin <Admin@DESKTOP-IJC4RIS>
2023-09-27 13:10:20 +0530

    1st commit

---
+++ src/main/java/com/GenericUtility/IAnnTrans.java
@@ -0,0 +1,18 @@
+package com.GenericUtility;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+import org.testng.IAnnotationTransformer;
+import org.testng.annotations.ITestAnnotation;
+
+public class IAnnTrans implements IAnnotationTransformer{
+
+	public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
+		annotation.setRetryAnalyzer(com.GenericUtility.RetryImpClass.class);
+	}
+
+	
+	
+
+}
