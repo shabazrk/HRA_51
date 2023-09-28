1f3ddb818e7751f7ac4b3241a0fdb9a035b40e2a
user <user@DESKTOP-D9IO8BK>
2023-09-28 11:25:38 +0530

    Ist Commit

--- src/test/java/practice/Admin.java
+++ src/test/java/practice/Admin.java
@@ -3,7 +3,7 @@ package practice;
 public class Admin {
 
 	public static void main(String[] args) {
-		System.out.println("Admin Task Completed");
+		System.out.println("--Admin Task Completed--");
 
 	}
 
