8eb7d62fe745fa091a418677bc9dccb17b872946
Acer <Acer@DESKTOP-L4VALVN>
2023-09-28 11:25:04 +0530

    IndividualHOME

--- src/test/java/practice/IndivvidualHomeRegistration.java
+++ src/test/java/practice/IndivvidualHomeRegistration.java
@@ -1,5 +1,11 @@
 package practice;
 
-public class IndivvidualHomeRegistration {
+import org.testng.annotations.Test;
 
+public class IndivvidualHomeRegistration {
+@Test
+public void IndividualHome() {
+	System.out.println("Individual Home Registration ");
+	System.out.println("Done Successfully");
+}
 }
